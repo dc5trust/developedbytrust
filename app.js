70227304f4a6e5a4b4b93701c4caabadb4761ece
Hector Bonilla <dc5trust@gmail.com>
2022-06-07 19:15:54 -0700

    burger menu and mobile menu added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello!');
+//Selected Items
+const lineOne = document.querySelector('.line-one')
+const lineTwo = document.querySelector('.line-two');
+const burgerMenu = document.querySelector('.mobile-menu');
+const popUp = document.querySelector('.mobile-menu-pop-up');
+
+//add Event Listeners 
+burgerMenu.addEventListener('click', openMobileMenu);
+
+function openMobileMenu (){
+    popUp.style.display = 'flex';
+
+}
