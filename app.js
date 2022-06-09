627ef0ed28a235d2910537bc177017652a4ca6fd
Hector Bonilla <dc5trust@gmail.com>
2022-06-08 18:40:47 -0700

    added about page, and some mobile designs

--- app.js
+++ app.js
@@ -3,11 +3,34 @@ const lineOne = document.querySelector('.line-one')
 const lineTwo = document.querySelector('.line-two');
 const burgerMenu = document.querySelector('.mobile-menu');
 const popUp = document.querySelector('.mobile-menu-pop-up');
+const navItems = document.querySelectorAll('#nav-items');
 
 //add Event Listeners 
 burgerMenu.addEventListener('click', openMobileMenu);
 
-function openMobileMenu (){
-    popUp.style.display = 'flex';
+navItems.forEach((items)=>{
+    items.addEventListener('click', ()=>{
+        //body position fixed removed 
+        // document.body.style.position = 'static';
+        // gsap.to(lineOne, {rotate: 0, x: '0', y: '0', duration: 1 });
+        // gsap.to(lineTwo, {rotate: 0, x: '0', y:'0', duration: 1});
+        //mobile pop up display hidden none
+    });
+});
 
+function openMobileMenu (){
+    
+    burgerMenu.classList.toggle('x-active');
+    if(burgerMenu.classList[1] === 'x-active'){
+        popUp.style.display = 'flex';
+        // document.body.style.position = 'fixed';
+        gsap.to(lineOne, {rotate: 45, x: '-5', y: '6', duration: 1});
+        gsap.to(lineTwo, {rotate: -45, x: '-5', y:'-6', duration: 1});
+    }else if(burgerMenu.classList[1] === undefined){
+        popUp.style.display = 'none';
+        // document.body.style.position = 'static';
+        gsap.to(lineOne, {rotate: 0, x: '0', y: '0', duration: 1 });
+        gsap.to(lineTwo, {rotate: 0, x: '0', y:'0', duration: 1});
+    }
+    
 }
